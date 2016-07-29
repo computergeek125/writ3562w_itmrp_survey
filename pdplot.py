d88f4991e12578d344422576aeb914c9332847d6
Ryan Smith <ryansmith125@gmail.com>
2016-07-29 18:54:55 -0500

    Added xbound and ybound control

--- pdplot.py
+++ pdplot.py
@@ -1,9 +1,19 @@
 import matplotlib
 import matplotlib.pyplot as plt
 
-def pdplot(pddata, kind='bar', xlabel=None, ylabel=None, title=None, **kwargs):
+def pdplot(pddata, kind='bar', xlabel=None, ylabel=None, xbound=[None, None], ybound=[None, None], title=None, **kwargs):
     ax = pddata.plot(kind=kind, **kwargs)
-    plt.axhline(0, color='k')
+    #plt.axhline(0, color='k')
+    if xbound:
+        if xbound[0]:
+            ax.set_xbound(lower=xbound[0])
+        if xbound[1]:
+            ax.set_xbound(upper=xbound[1])
+    if ybound:
+        if ybound[0]:
+            ax.set_ybound(lower=ybound[0])
+        if ybound[1]:
+            ax.set_ybound(upper=ybound[1])
     if xlabel:
         ax.set_xlabel(xlabel)
     if ylabel:
