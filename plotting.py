1ed90c7df92d72915fa7cc719d2363881d230020
Ryan Smith <ryansmith125@gmail.com>
2016-07-28 20:41:51 -0500

    Implemented generic bar grapher from the plot_mc method

--- plotting.py
+++ plotting.py
@@ -9,6 +9,86 @@ import plotting_util as u
 # Init Matplotlib
 mpl.rcParams['backend'] = "TkAgg"
 
+def barchart(data, legend=[], label_length=15, label_clip=-1, title=None, xlabel=None, ylabel=None, 
+    xtick_rotation=-45, xtick_labels=None, group_width=0.7, color=None, alpha=0.5):
+    # data should be a list of lists: each sub-list is a series in the data, and each item in the sublists is a bar
+    # based on: http://matplotlib.org/examples/api/barchart_demo.html
+    fig, ax = plt.subplots()
+    try:
+        a = data[0][0]
+    except TypeError:
+        data = [data]
+    pN = len(data[0])
+    dlen = len(data)
+    bar_width = group_width/dlen
+    xt = np.arange(pN)  # the x locations for the groups
+    if color:
+        if type(color) is tuple or type(color) is list:
+            if len(color) < dlen:
+                raise RuntimeError("If you specify a list of colors, the length must match the number of series in data")
+        elif type(color) is str:
+            cmap = color
+            color = u.mp_get_cmap(dlen, cmap=cmap)
+        else:
+            color = [color]*dlen # kluge but it works...
+    else:
+        color = u.mp_get_cmap(dlen)
+    rects = []
+    for i in range(len(data)):
+        rects.append(ax.bar(xt+bar_width*i, data[i], bar_width, color=color(i), alpha=alpha))
+
+    # add some text for labels, title and axes ticks
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    if title:
+        ax.set_title(title, fontsize="xx-large", y=1.04)
+    
+    mb = None
+    for i in data:
+        lm = max(i)
+        if mb is None:
+            mb = lm
+        elif lm > mb:
+            mb = lm
+    ax.set_xbound(lower=xt[0]+group_width/2-0.5, upper=xt[-1]+group_width/2+0.5)
+    ax.set_ybound(upper=mb*1.07)
+    ax.set_xticks(xt + group_width/2)
+    ax.autoscale(enable=False, axis="x", tight=True)
+
+    if xtick_labels:
+        names = xtick_labels
+        labels_clipped = [u.label_clipper(text,label_clip) for text in names]
+        labels = [textwrap.fill(text,label_length) for text in labels_clipped]
+        bot_off = (-np.sin(np.radians(xtick_rotation))) * (label_length * 0.016)
+        fig.subplots_adjust(bottom=bot_off)
+    else:
+        labels = [""]*dlen
+    ax.set_xticklabels(labels, rotation=xtick_rotation)
+
+    if dlen > 1:
+        lr = []
+        for i in rects:
+            lr.append(i[0])
+        llen = len(legend)
+        if llen < dlen:
+            for i in range(llen-1, dlen):
+                legend.append("Series {0}".format(i+1))
+        lmax = 0
+        for i in legend:
+            li = len(i)
+            if li > lmax:
+                lmax = li
+        ax.legend(lr, legend, bbox_to_anchor=(1,1), bbox_transform=fig.transFigure)
+        fig.subplots_adjust(left=0.05, right=1-(0.016*li+0.05))
+    
+    for i in rects:
+        u.mp_autolabel(i, ax)
+
+    plt.show(block=False)
+    return (fig, ax)
+
 def plot_mc(data, legend=[], label_length=15, label_clip=-1, title=None, xlabel=None, ylabel=None, 
     xtick_rotation=-45, xtick_labels=None, group_width=0.7, color=None, alpha=0.5):
     # data should be of format: {'keys': ['1', '2', '3', '4', '5'], 'names': ['One', 'Two', 'Three', 'Four', 'Five'], 'bars': [5, 13, 1, 1, 0]}
