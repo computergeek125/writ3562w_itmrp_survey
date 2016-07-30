e4df2c84cfe7a4a95d45c64fcd92f01ef3b6cf89
Ryan Smith <ryansmith125@gmail.com>
2016-07-30 16:34:47 -0500

    Added some override paramaters

--- pdplot.py
+++ pdplot.py
@@ -1,7 +1,16 @@
 import matplotlib
 import matplotlib.pyplot as plt
+import textwrap
 
-def pdplot(pddata, kind='bar', xlabel=None, ylabel=None, xbound=[None, None], ybound=[None, None], title=None, **kwargs):
+import plotting_util as u
+
+def pdplot(pddata, kind='bar', title=None, xlabel=None, ylabel=None, legend=[],
+    xrot=-45, xtick_labels=None, label_length=15, label_clip=-1,
+    xbound=[None, None], ybound=[None, None],  **kwargs):
+    kwargs['rot'] = xrot
+    #if type(data) is tuple or type(data) is list:
+    #    pass
+    #else:
     ax = pddata.plot(kind=kind, **kwargs)
     #plt.axhline(0, color='k')
     if xbound:
@@ -20,4 +29,18 @@ def pdplot(pddata, kind='bar', xlabel=None, ylabel=None, xbound=[None, None], yb
         ax.set_ylabel(ylabel)
     if title:
         ax.set_title(title, fontsize="xx-large", y=1.04)
+
+
+    if xtick_labels:
+        names = xtick_labels
+    else:
+        names = pddata.index
+    labels_clipped = [u.label_clipper(text,label_clip) for text in names]
+    labels = [textwrap.fill(text,label_length) for text in labels_clipped]
+    ax.set_xticklabels(labels)
+
+    if legend:
+        ax.legend(legend, loc='best')
     return ax
+
+#%def nars_graphby(nars_s1, nars_s2, nars_s3, question)
