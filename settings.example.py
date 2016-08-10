1cefdec7f531661a0ad737e183a6023dd8bff832
Ryan Smith <ryansmith125@gmail.com>
2016-08-09 21:03:51 -0500

    Added example settings file

---
+++ settings.example.py
@@ -0,0 +1,17 @@
+qualtrics_datacenter = "umn.qualtrics.com"
+qualtrics_api_key = "<your key here>"
+qualtrics_survey = "<your survey here>"
+
+nars_s1 = ["Q5.1_4", "Q5.1_7", "Q5.1_8", "Q5.1_9", "Q5.1_10", "Q5.1_12"]
+nars_s2 = ["Q5.1_1", "Q5.1_2", "Q5.1_11", "Q5.1_13", "Q5.1_14"]
+nars_s3 = ["Q5.1_3", "Q5.1_5", "Q5.1_6"]
+nars_s2_amd = []
+nars_s3_amd = ["Q5.1_3"]
+
+nars_itmrp_s1 = ['Q6.2_2', 'Q6.2_3', 'Q6.2_4', 'Q6.2_5', 'Q6.2_6', 'Q6.2_7']
+nars_itmrp_s2 = []
+nars_itmrp_s3 = ['Q6.2_1']
+
+cQ2_4_short = ['Basic', 'Knowledgeable', 'Enthusiast', 'Professional', 'Other']
+cQ2_5_short = ['N/A', 'MMO', 'MOBA', 'RPG', 'RTS', 'FPS-style', 'Sandbox / Open World', 'Action / Adventure', 'Interactive Fiction', 'Casual', 'Other']
+cQ3_1_short = ['Family member', 'Friend', 'Co-worker', 'Professional', 'Solve it on your own', 'I am professional IT', 'Other']
